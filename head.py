b2ac94badce56717a996116818907f103b8d7376
Ferenc Vajda <github@fercsi.com>
2023-10-18 08:33:16 +0000

    Modify test code (head.py)

--- head.py
+++ head.py
@@ -3,16 +3,24 @@
 import tls
 
 def main():
-    client = tls.Client(
-        hostname='portal.fercsi.com',
-#>        key_share_group = 'x448',
-#>        key_share_group = 'secp256r1',
-#>        key_share_group = 'secp384r1',
-        key_share_group = 'secp521r1',
-        )
-    client.connect()
-    client.send("HEAD / HTTP/1.1\r\nhost: portal.fercsi.com\r\nconnection: close\r\n\r\n")
-    print(client.receive())
+    try:
+        client = tls.Client(
+#>            hostname='portal.fercsi.com',
+            hostname='www.google.com',
+#>            hostname='www.telex.hu',
+#>            key_share_group = 'x448',
+#>            key_share_group = 'secp256r1',
+#>            key_share_group = 'secp384r1',
+#>            key_share_group = 'secp521r1',
+#>            key_share_group = 'ffdhe2048',
+#>            debug_level = 10,
+            )
+        client.connect()
+        client.send("HEAD / HTTP/1.1\r\nhost: portal.fercsi.com\r\nconnection: close\r\n\r\n")
+        print(client.receive())
+    except Exception as e:
+#>        print(e)
+        raise(e)
 
 if __name__ == "__main__":
     main()
