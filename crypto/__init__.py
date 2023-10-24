5e4e2f760c7c715d8fd923878a8dd9ef130db36d
Ferenc Vajda <github@fercsi.com>
2023-10-24 14:21:30 +0000

    integrate AES with system

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from .encryption import Encryption, NoEncryption
-from .aes import AES
 from .gcm import GCM
 from .aesgcm import AES_GCM
 from .hkdf import HKDF
--- crypto/aesgcm.py
+++ crypto/aesgcm.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 from .aead import AEAD
-from .aes import AES
+from .cipher.aes import aes
 from .gcm import GCM
 
 class AES_GCM(AEAD):
     def encrypt_message(self, plain_text: bytes, auth_data: bytes, nonce: bytes) -> (bytes, bytes):
-        gcm = GCM(AES(self.my_key))
+        gcm = GCM(aes(self.my_key))
         return gcm.encrypt(plain_text, auth_data, nonce)
 
     def decrypt_message(self, cipher_text: bytes, auth_data: bytes, nonce: bytes) -> (bytes, bytes):
-        gcm = GCM(AES(self.peer_key))
+        gcm = GCM(aes(self.peer_key))
         return gcm.decrypt(cipher_text, auth_data, nonce)
