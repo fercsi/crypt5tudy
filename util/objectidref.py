08ecd24a317de5b067e28ef9dcdd17f031c21b9b
Ferenc Vajda <github@fercsi.com>
2023-11-14 16:21:56 +0000

    Add new OID entries

--- util/objectidref.py
+++ util/objectidref.py
@@ -2,6 +2,10 @@
 
 OBJECT_ID_REFERENCE = {
     # iso(1) member-body(2) us(840)
+    '1.2.840.10040': 'x9-57',
+    '1.2.840.10040.4': 'x9algorithm',
+    '1.2.840.10040.4.1': 'dsa',
+    '1.2.840.10040.4.3': 'dsa-with-sha1',
     '1.2.840.113549': 'rsadsi',
     '1.2.840.113549.1': 'pkcs',
     '1.2.840.113549.1.1': 'pkcs-1',
@@ -13,16 +17,53 @@ OBJECT_ID_REFERENCE = {
     '1.2.840.113549.1.1.12': 'sha384WithRSAEncryption',
     '1.2.840.113549.1.1.13': 'sha512WithRSAEncryption',
     '1.2.840.113549.1.1.14': 'sha224WithRSAEncryption',
+    '1.2.840.113549.1.7': 'pkcs-7',
+    '1.2.840.113549.1.7.1': 'data',
+    '1.2.840.113549.1.7.3': 'envelopedData',
+    '1.2.840.113549.1.7.6': 'encryptedData',
+    '1.2.840.113549.1.9': 'pkcs-9',
+    '1.2.840.113549.1.9.1': 'emailAddress',
+    '1.2.840.113549.1.9.2': 'unstructuredName',
+    '1.2.840.113549.1.12': 'pkcs-12',
+    '1.2.840.113549.1.12.1': 'pkcs-12PbeIds',
+    '1.2.840.113549.1.12.1.3': 'pbeWithSHAAnd3-KeyTripleDES-CBC',
+    '1.2.840.113549.1.12.10': 'pkcs-12Version1',
+    '1.2.840.113549.1.12.10.1': 'pkcs-12BagIds',
+    '1.2.840.113549.1.12.10.1.2': 'pkcs-8ShroudedKeyBag',
 
     # iso(1) org(3) oiw(14) secsig(3) algorithms(2)
     '1.3.14.3.2.12': 'dsa',
+    '1.3.14.3.2.26': 'hashAlgorithmIdentifier',
     '1.3.14.3.2.27': 'dsaWithSHA1',
 
-    # joint-iso-itu-t(2) ds(5) attributeType(4)
+    # iso(1) org(3) thawte(101)
+    '1.3.101.110': 'id-X25519',
+    '1.3.101.111': 'id-X448',
+    '1.3.101.112': 'id-Ed25519',
+    '1.3.101.113': 'id-Ed448',
+    '1.3.101.112': 'id-EdDSA25519-ph', #prehashing
+    '1.3.101.113': 'id-EdDSA448',
+
+    # joint-iso-itu-t(2) ds(5)
+    '2.5.4': 'attributeType',
+    # Followung attributes can be used in certificates:
     '2.5.4.3': 'commonName', # CN
+    '2.5.4.4': 'surname', # SN
+    '2.5.4.5': 'serialNumber',
     '2.5.4.6': 'countryName', # C
     '2.5.4.7': 'localityName', # L
-    '2.5.4.8': 'stateOrProvinceName', # S
+    '2.5.4.8': 'stateOrProvinceName', # ST
     '2.5.4.10': 'organizationName', # O
     '2.5.4.11': 'organizationUnitName', # OU
+    '2.5.4.12': 'title',
+    '2.5.4.42': 'givenName',
+    '2.5.4.43': 'initials',
+    '2.5.4.44': 'generationQualifier',
+    '2.5.4.46': 'dnQualifier', # distinguished name qualifier
+    '2.5.4.65': 'pseudonym',
+
+    '2.5.29': 'certificateExtension',
+    '2.5.29.14': 'subjectKeyIdentifier',
+    '2.5.29.19': 'basicConstraints',
+    '2.5.29.35': 'authorityKeyIdentifier',
 }
